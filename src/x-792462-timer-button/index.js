54b3e251e6194ff7fef3d8dece251d5fe0693206
Nick Allen <nallen@esolutionsone.com>
2022-08-08 15:35:45 -0500

    import button and include react-error-overlay devDependency

--- src/x-792462-timer-button/index.js
+++ src/x-792462-timer-button/index.js
@@ -1,15 +1,23 @@
 import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
+import '@servicenow/now-button';
 import styles from './styles.scss';
 
 const view = (state, {updateState}) => {
 	return (
-		<div></div>
+		<now-button>{state.previousTime}</now-button>
 	);
 };
 
 createCustomElement('x-792462-timer-button', {
 	renderer: {type: snabbdom},
 	view,
-	styles
+	styles,
+	initialState: {
+		previousTime: 0,
+	},
+	actionHandlers: {
+		'NOW_BUTTON#CLICKED': ({action}) => console.log('clicked'),
+		'TIMER_BUTTON#CLICKED': ({action}) => console.log(action.payload),
+	}
 });
