5eeca14cba0aa5ebf9692a606256d807a6241830
Nick Allen <nallen@esolutionsone.com>
2022-08-15 09:43:57 -0500

    set initialState dates to null

--- src/x-792462-timer-button/index.js
+++ src/x-792462-timer-button/index.js
@@ -2,16 +2,16 @@ import {createCustomElement, actionTypes} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import '@servicenow/now-button';
 import styles from './styles.scss';
-import { differenceInMilliseconds, intervalToDuration } from 'date-fns';
+import { differenceInMilliseconds, intervalToDuration, parseISO } from 'date-fns';
 
 const {COMPONENT_BOOTSTRAPPED} = actionTypes;
 
 const difference = (current, load) => {
+	if(!current || !load) return '00:00:00';
 	let duration = intervalToDuration({
 		start: 0,
 		end: differenceInMilliseconds(current, load) || 0,
 	});
-
 
 	let {hours, minutes, seconds} = duration;
 
@@ -23,6 +23,7 @@ const difference = (current, load) => {
 			return num;
 		}
 	}
+
 	return `${leadZeroString(hours)}:${leadZeroString(minutes)}:${leadZeroString(seconds)}`;
 }
 
@@ -52,11 +53,11 @@ createCustomElement('x-792462-timer-button', {
 	initialState: {
 		previousTime: 0,
 		seconds: 0,
-		currentTime: 'init',
-		loadTime: 'init',
+		currentTime: null,
+		loadTime: null,
 	},
 	actionHandlers: {
-		[COMPONENT_BOOTSTRAPPED]: ({updateState}) => updateState({loadTime: new Date()}),
+		[COMPONENT_BOOTSTRAPPED]: ({updateState}) => updateState({loadTime: new Date()}), // replace with REST
 		'NOW_BUTTON#CLICKED': ({action}) => console.log('clicked'),
 		'TIMER_BUTTON#CLICKED': ({action}) => console.log(action.payload),
 	}
