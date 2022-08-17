178eab37f42f7e51405d4f717972e6c1e8437728
Nick Allen <nallen@esolutionsone.com>
2022-08-17 11:58:30 -0500

    init styles

--- src/x-792462-timer-button/view.js
+++ src/x-792462-timer-button/view.js
@@ -1,52 +1,62 @@
 import { Fragment } from '@servicenow/ui-renderer-snabbdom';
-import { 
-    difference, 
-    getUTCTime, 
-    stringifyDuration, 
-    roundDuration 
+import {
+    difference,
+    getUTCTime,
+    stringifyDuration,
+    roundDuration
 } from './helpers';
 
-export const view = (state, {updateState, dispatch }) => {
-	const {properties, currentTime} = state;
-	const { active, start } = properties; 
+export const view = (state, { updateState, dispatch }) => {
+    const { properties, currentTime } = state;
+    const { active, start } = properties;
     const style = { color: active == "true" ? 'green' : 'red' };
+    const isActive = active === "true";
 
-	// Update every second
-	let interval = null;
-    if ( active == "true" ) {
-      interval = setInterval(() => {
-        updateState({currentTime: new Date()});
-		clearInterval(interval);
-      }, 1000);
-    } else if (active != "true") {
-      clearInterval(interval);
+
+    // Update every second
+    let interval = null;
+    if (isActive) {
+        interval = setInterval(() => {
+            updateState({ currentTime: new Date() });
+            clearInterval(interval);
+        }, 1000);
+    } else if (!isActive) {
+        clearInterval(interval);
     }
 
-    const timerDuration = start ? difference(currentTime, getUTCTime(start)) 
-        : {hours: 0, minutes: 0, seconds: 0};
+    const timerDuration = start ? difference(currentTime, getUTCTime(start))
+        : { hours: 0, minutes: 0, seconds: 0 };
     const roundedDuration = roundDuration(timerDuration);
     const timerDisplayValue = stringifyDuration(timerDuration);
-	
-	return (
-		<Fragment>
-			<button 
-			style={style}
-			on-click={() => {
-				const {timestampTable } = properties;
-				if(active !==  "true"){
-					dispatch('INSERT_TIMESTAMP', {
-						timestampTable,
-						data: { active: true }
-					})
-				}else{
-					// should we call this OPEN_ and CLOSE_TIMESTAMP?
-					dispatch('UPDATE_TIMESTAMP', { timestampTable, 
-						sys_id: properties.sysId,
-						data: { active: false } 
-					});
-				}
-			}
-			}>{timerDisplayValue || 'Start'}</button>
-		</Fragment>
-	);
+
+    const timerStart = () => {
+        const { timestampTable } = properties;
+        if (!isActive) {
+            dispatch('INSERT_TIMESTAMP', {
+                timestampTable,
+                data: { active: true }
+            })
+        } else {
+            // should we call this OPEN_ and CLOSE_TIMESTAMP?
+            dispatch('UPDATE_TIMESTAMP', {
+                timestampTable,
+                sys_id: properties.sysId,
+                data: { active: false }
+            });
+        }
+    }
+
+    return (
+        <Fragment>
+            <div className="timer-container">
+                <span className={isActive ? 'display-active' : 'display-inactive'}>
+                    {timerDisplayValue || 'Start'}
+                </span>
+                <button
+                    style={style}
+                    on-click={timerStart}></button>
+            </div>
+			<button></button>
+        </Fragment>
+    );
 };
