8283d4cb78a9eb89c4e4a4dd73d0bc1716cb331e
Nick Allen <nallen@esolutionsone.com>
2022-08-19 11:42:05 -0500

    remove preview json

--- src/x-esg-timer-container/view.js
+++ src/x-esg-timer-container/view.js
@@ -6,7 +6,7 @@ export const view = (state) => {
 const {projects} = state
     return (
         <Fragment>
-            <pre>{JSON.stringify(projects, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(projects, null, 2)}</pre> */}
             {projects.map(proj => <x-esg-timer-button projectData={proj}/>)}
         </Fragment>
     );
