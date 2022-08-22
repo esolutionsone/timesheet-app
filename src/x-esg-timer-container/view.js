3ee1d0741a051a9e285efa017dd7c70886aced73
Nick Allen <nallen@esolutionsone.com>
2022-08-22 15:55:43 -0500

    minor styling changes

--- src/x-esg-timer-container/view.js
+++ src/x-esg-timer-container/view.js
@@ -12,37 +12,35 @@ export const view = (state, {dispatch, updateState}) => {
     })
     const {projects, selectedProject, consultantId, entryNotes, genericProjects} = state;
     const allProjects = [...genericProjects, ...projects];
-    console.log('selectedProject', state.consultantId)
-    console.log('generic_projects', state.genericProjects)
     console.log(allProjects)
     return (
         <Fragment>
             {/* <pre>{JSON.stringify(projects, null, 2)}</pre> */}
-            <div>
-            <select on-change={(e)=>updateState({selectedProject: e.target.value})}>
-                <option disabled selected>Choose a Project</option>
-                {allProjects.map(proj => <option value={proj.sys_id}>
-                    {proj.short_description}
-                </option>)}
-            </select>
-            <textarea 
-                on-keyup={(e)=> updateState({entryNotes: e.target.value})}
-                maxlength='512'
-            ></textarea>
-            <span 
-                className="material-symbols-outlined"
-                on-click={() => dispatch('NEW_ENTRY', {
-                    data: {
-                        project: selectedProject,
-                        consultant: consultantId,
-                        note: entryNotes,
-                    },
-                    tableName: 'x_esg_one_delivery_time_entry',
-                })}    
-            >
-                add_circle_outline
-            </span>
-            </div>
+            <form>
+                <select on-change={(e)=>updateState({selectedProject: e.target.value})}>
+                    <option disabled selected>Choose a Project</option>
+                    {allProjects.map(proj => <option value={proj.sys_id}>
+                        {proj.short_description}
+                    </option>)}
+                </select>
+                <textarea 
+                    on-keyup={(e)=> updateState({entryNotes: e.target.value})}
+                    maxlength='512'
+                ></textarea>
+                <span 
+                    className="material-symbols-outlined"
+                    on-click={() => dispatch('NEW_ENTRY', {
+                        data: {
+                            project: selectedProject,
+                            consultant: consultantId,
+                            note: entryNotes,
+                        },
+                        tableName: 'x_esg_one_delivery_time_entry',
+                    })}    
+                >
+                    add_circle_outline
+                </span>
+            </form>
             <hr></hr>
             {projects.map(proj => <x-esg-timer-button 
                 projectData={proj}
