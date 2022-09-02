c880fe661fdfc590852456d305ea856ad00581c6
Nick Allen <nallen@esolutionsone.com>
2022-09-02 13:11:36 -0500

    week view is now navigable week to week

--- src/x-esg-week-view/view.js
+++ src/x-esg-week-view/view.js
@@ -1,19 +1,17 @@
 import { WeeklyHeader } from "./components/WeeklyHeader";
 import { WeeklySubHeader } from "./components/WeeklySubHeader";
 
-export const view = (state) => {
+export const view = (state, {updateState}) => {
 
     const { selectedDay, projectMap, dailyEntries } = state
 
     console.log("STATE", state);
-
     
-    
-
 	return (
         <div className="week-container">
             <WeeklyHeader 
                 selectedDay={selectedDay}
+                updateState={updateState}
             />
             <WeeklySubHeader
                 selectedDay={selectedDay}
