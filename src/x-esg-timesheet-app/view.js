f1e37fbfd6b631c45bd2cdd914c59ada356069be
Alfredo reza <alfredoreza@alfredos-mbp-2.lan>
2022-09-07 13:36:06 -0500

    remove margins on container, remove buttons from week view

--- src/x-esg-timesheet-app/view.js
+++ src/x-esg-timesheet-app/view.js
@@ -60,25 +60,29 @@ export default (state, {updateState}) => {
 	return (
 		<div>
 			<div className="outer-buttons">
-				<div className="add-edit-buttons">
-					<button 
-						className="add-project-button"
-						on-click={()=>updateState({
-										addProjectStatus: !addProjectStatus, 
-										editMode: false
-									})
-								}>
-							<span className="material-symbols-outlined">add</span>
-							Time Entry
-					</button>
-					<button 
-						className="edit-button"
-						on-click={()=>updateState({editMode: !editMode})}>
-							<span className="material-symbols-outlined">edit_square</span>
-							Edit
-					</button>
-				</div>
-				<div>
+				{location == 'day' ?
+					<div className="add-edit-buttons">
+						<button 
+							className="add-project-button"
+							on-click={()=>updateState({
+											addProjectStatus: !addProjectStatus, 
+											editMode: false
+										})
+									}>
+								<span className="material-symbols-outlined">add</span>
+								Time Entry
+						</button>
+						<button 
+							className="edit-button"
+							on-click={()=>updateState({editMode: !editMode})}>
+								<span className="material-symbols-outlined">edit_square</span>
+								Edit
+						</button>
+					</div> 
+					: 
+					<div></div>
+				}
+				<div className={`${(location == 'week') ? 'timer-week-showing' : ''}`}>
 					<button 
 						className={`day-button ${(location == 'day') ? 'active' : ''}`}
 						on-click={()=> updateState({location: 'day', addProjectStatus: false, editMode: false})}>
@@ -92,10 +96,6 @@ export default (state, {updateState}) => {
 				</div>
 			</div>
 			{jsx}
-			<div >
-
-			</div>
 		</div>
-		
 	)
 }
