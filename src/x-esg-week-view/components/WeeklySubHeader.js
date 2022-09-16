418f15684a58eb11491d51ad068c18709d3bfc11
Nick Allen <nallen@esolutionsone.com>
2022-09-16 10:42:07 -0500

    clean and comment WeeklySubheader

--- src/x-esg-week-view/components/WeeklySubHeader.js
+++ src/x-esg-week-view/components/WeeklySubHeader.js
@@ -2,21 +2,18 @@ import { getSnDayBounds, getUTCTime, getWeekBounds } from "../../helpers";
 import { format } from "date-fns";
 
 export const WeeklySubHeader = ({
-    selectedDay, 
-    // dailyEntries,
+    selectedDay,
     dateArr,
     entries,
     timestamps
 }) => {
-    console.log(timestamps)
-    console.log(entries);
-    // const projects = Array.from(projectMap.values());
-
-
     return (
         <div className="weekly-subheader week-view-grid">
             <div className="weekly-subheader-title">Projects</div>
-            {dateArr.map(date => {
+
+            { // For each day of the week:
+            dateArr.map(date => {
+
                 // Filter timestamps by day
                 const bounds = getSnDayBounds(date);
                 let dailyTimestamps = timestamps.filter(stamp => {
@@ -24,35 +21,24 @@ export const WeeklySubHeader = ({
                         && stamp.start_time < bounds[1];
                 })
 
-                console.log('TIMESTMAPS', dailyTimestamps)
-                // projects.forEach(proj => {  
-                //     if(proj.timestamps){
-                //         dailyTimestamps = dailyTimestamps.concat(
-                //             proj.timestamps.filter(stamp => {
-                //                 return stamp.start_time > bounds[0] 
-                //                     && stamp.start_time < bounds[1];
-                //             })
-                //         );
-                //     }  
-                // })
-
+                // Add the ms for all the daily timestamps
                 const totalTime = dailyTimestamps.reduce((ms, stamp) => {
                     if(stamp.rounded_duration == '') return ms;
                     return ms + getUTCTime(stamp.rounded_duration).getTime();
                 }, 0)
 
-                // The following code theoretically works,
-                // but we'll have to adjust how we handle weekly time input and negative values
-                           
+                // Filter the entries by day
                 const timeEntries = entries.filter(entry => {
                     return entry.date == bounds[0].split(' ')[0]
                         && entry.time_adjustment.length > 0;
                 })
 
+                // Add the ms for all the daily entries
                 const adjustmentMs = timeEntries.reduce((ms, entry) => {
                     return ms + getUTCTime(entry.time_adjustment).getTime();
                 }, 0);
 
+                // Convert to hours
                 const totalHours = (totalTime + adjustmentMs) / 1000 / 60 / 60;
 
                 return <div>
@@ -63,7 +49,6 @@ export const WeeklySubHeader = ({
                         >
                             {totalHours}
                         </div>
-                        {/* <div>{JSON.stringify(dailyTimestamps, null, 2)}</div> */}
                     </div>
             })}
         </div>
